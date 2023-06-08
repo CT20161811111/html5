3113f2e82af95b1c6a677ce39cf17227722a75be
CT20161811111 <navyaswamy63@gmail.com>
2023-06-08 17:42:53 +0530

    html-5

---
+++ p6.js
@@ -0,0 +1,17 @@
+{/* <script>
+        let minVariable= 1;
+        function startProgress(){
+            if(minVariable<=100){
+                minVariable++;
+                document.getElementById("progress").value= minVariable;
+            }
+        }
+        setInterval("startProgress()",2000);
+    </script> */}
+
+    let minVariable= 1;
+    function startFunction(){
+        if(minVariable<=100){
+            minVariable++
+           
+    }setInterval("startFunction(),1000")
